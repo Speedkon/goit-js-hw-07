d2958798788ee7bae1ec40b8f4f89968125ed986
Speedkon <katakononihina@gmail.com>
2023-09-14 22:19:01 +0300

    work2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(item => `
+<li class="gallery__item" data-preview=${item.preview}>
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>
+`).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightboxModal = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDeay: 250,
+});
